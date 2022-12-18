bf71af8f6b4854ba4586db5426f47fc19f25d780
Thomas905 <thomasclarisse03@gmail.com>
2022-12-18 10:59:39 +0100

    add dotenv lib

--- router/router.js
+++ router/router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const dotenv = require('dotenv');
+dotenv.config(process.cwd(), '.env');
 
 router.use('/test', function(req, res, next) {
     res.send('Hello world de test');
